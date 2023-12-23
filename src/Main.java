3b7fb76fc82c3bd62430790e79565ec9d8576a5b
Технолог ДСС <petrzolotov04121978@yandex.ru>
2023-12-23 12:00:33 +0500

    Проверьте ДЗ

---
+++ src/Main.java
@@ -0,0 +1,109 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задание 1");
+        var dog = 8;
+
+            System.out.println(dog);
+
+        var cat = 3.6;
+
+            System.out.println(cat);
+
+        var paper = 763789;
+
+            System.out.println(paper);
+
+
+        System.out.println("Задание 2");
+        var dog1 = 8;
+
+            System.out.println(dog1 + 2);
+
+        var cat1 = 3.6;
+        System.out.println(cat1 + 2);
+
+        var paper1 = 763789;
+
+            System.out.println(paper1 + 2);
+
+
+        System.out.println(" Задание 3");
+
+        var dog2 = 8;
+
+            System.out.println(dog2- 3.5);
+
+        var cat2 = 3.6;
+
+            System.out.println(cat2 - 1.6);
+
+        var paper2 = 763789;
+
+            System.out.println(paper2 - 3639);
+
+
+        System.out.println("Задание 4");
+        var friend = 19;
+
+            System.out.println(friend);
+
+
+            System.out.println(friend + 2);
+
+
+            System.out.println((friend + 2) / 7);
+
+
+        System.out.println("Задание 5");
+        var frog = 3.5;
+
+            System.out.println(frog);
+
+
+            System.out.println(frog * 10);
+
+
+            System.out.println((frog * 10) / 3.5);
+
+
+            System.out.println((frog * 10) / 3.5 + 4);
+
+        System.out.println("Задание 6");
+        var weight1 = 78.2;
+        var weight2 = 82.7;
+        var result1 = weight1 + weight2;
+        var result2 = weight2 - weight1;
+
+
+            System.out.println(" Общая масса бойцов" + result1 + "кг");
+
+
+            System.out.println("Разница в весе" + result2 + "кг");
+
+
+        System.out.println("Задание 7");
+        var weight11= 78.2;
+        var weight22 = 82.7;
+        var result11 = weight22 - weight11;
+        var result22 = weight22 % weight11;
+
+            System.out.println("Разница в весе" + result11 + "кг");
+
+
+            System.out.println("Разница в весе" + result22 + "кг");
+
+
+        System.out.println("Задание 8");
+        var time = 640;
+        var timeOneWorker = 8;
+        var allWorker = time / timeOneWorker;
+
+            System.out.println("Всего работников в компании - " + allWorker);
+
+        var allWoker2 = allWorker + 94;
+        var time2 = allWoker2 * timeOneWorker;
+
+            System.out.println("Если в компании работает"   +allWoker2 + ",то всего"   + time2 +   "может быть использовано");
+
+    }
+}
